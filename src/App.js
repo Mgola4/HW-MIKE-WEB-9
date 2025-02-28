15dcc7e6febd62a0abbdd8e334c05ec58967d4b0
Mike Gola <mgola093@gmail.com>
2025-02-28 01:17:36 -0500

    New Commit With React Mods

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Fun and Easy
         </a>
       </header>
     </div>
